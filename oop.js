cd446f44a3697d170f1617359bd9a7e1a8724d8a
Mohsen Farahan <faramohsen@Mohsens-MacBook-Pro.local>
2019-07-09 19:10:08 +0400

    add class

--- oop.js
+++ oop.js
@@ -1,15 +1,23 @@
 console.log("hello");
 grades = [14, 12, 9, 15, 7, 17, 19, 16, 18, 19.5, 16.5];
 
-function getBestGrade(grades) {
-  if (grades.length <= 0) return null;
-  let max = grades[0];
-  for (let i = 1; i < grades.length; i++) {
-    if (grades[i] > max) {
-      max = grades[i];
-    }
+class Student {
+  constructor(grades) {
+    this.grades = grades;
+    console.log(grades);
   }
-  return max;
+
+  getBestGrade() {
+    if (this.grades.length <= 0) return null;
+    let max = this.grades[0];
+    for (let i = 1; i < this.grades.length; i++) {
+      if (this.grades[i] > max) {
+        max = this.grades[i];
+      }
+    }
+    return max;
+  }
 }
 
-console.log("best grade: " + getBestGrade(grades));
+const carlos = new Student(grades);
+console.log(carlos.getBestGrade());
